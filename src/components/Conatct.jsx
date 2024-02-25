e452465b59308d656797d3de4d17db9f4fe752e6
SawantAchal <achals1718@gmail.com>
2024-02-25 20:48:48 +0530

    mount form component

---
+++ src/components/Conatct.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ContactForm from './ContactForm'
+
+const Conatct = () => {
+  return (
+    <div className='items-center  '>
+      <h1 className='text-3xl text-center mt-2 mb-3'>Contact Form</h1>
+      <ContactForm/>
+    </div>
+  )
+}
+
+export default Conatct
+
+
+
